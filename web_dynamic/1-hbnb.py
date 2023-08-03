87681496051b60bd24565ebd40c16d1704b90f66
Dev-wonderful <adeonederful20@gmail.com>
2023-08-03 14:43:49 +0100

    data

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -34,6 +34,7 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    print(places.__dict__)
 
     return render_template('1-hbnb.html',
                            states=st_ct,
@@ -44,4 +45,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
