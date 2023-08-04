ca7bcf2177fea738209e8ffcf3351535033bd229
6godpro <erickson5115@gmail.com>
2023-08-04 14:33:52 +0100

    (new)-> new status icon feature on titlebar included, ###ff545f if active and #ccc otherwise

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$('document').ready( function () {
+  // Everything goes here
+  const checked = [];
+  $('.amenities input[data-id]').on('click', function (event) {
+    const target = event.target;
+    if (target.checked === true) {
+      console.log(target.dataset.name)
+      checked.push(target.dataset.name);
+    } else {
+      checked.splice(checked.indexOf(target.dataset.name), 1);
+    }
+    const filter = checked.join(', ')
+    $('DIV.amenities h4').text(filter)
+  });
+
+  // request the endpoint to check status
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
+    if (data['status'] === 'OK') {
+      console.log(data);
+      $('DIV#api_status').addClass('available')
+    }
+    else {
+      $('DIV#api_status').removeClass('available')
+    }
+  });
+})
