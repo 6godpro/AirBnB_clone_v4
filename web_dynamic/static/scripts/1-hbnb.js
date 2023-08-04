6884ef0dd06144eed42caf7b8c402fb30e4d2dc8
Dev-wonderful <adeonederful20@gmail.com>
2023-08-04 11:43:53 +0100

    html data attribute breaking names by spaces fixed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,6 +4,7 @@ $('document').ready( function () {
   $('.amenities input[data-id]').on('click', function (event) {
     const target = event.target;
     if (target.checked === true) {
+      console.log(target.dataset.name)
       checked.push(target.dataset.name);
     } else {
       checked.splice(checked.indexOf(target.dataset.name), 1);
